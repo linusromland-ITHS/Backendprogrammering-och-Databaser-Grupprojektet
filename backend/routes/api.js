fc61317fdff78ad4dece8a0d19f62e9ba6627e92
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-04-21 16:28:46 +0200

    Update api.js

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/currency', require('./currency.routes'));
+
 router.get('/', async (req, res) => {
     res.json({
         success: true,
