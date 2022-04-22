55c9fd38fb55f8e7b5868fe2a619d6115e3e33f0
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-04-22 19:53:06 +0200

    created currency PUT route

--- backend/routes/currency.routes.js
+++ backend/routes/currency.routes.js
@@ -39,4 +39,47 @@ router.post('/', async (req, res) => {
     }
 });
 
+/**
+ * @api {put} /api/currency/ Edits the currency with the given id
+ */
+router.put('/', async (req, res) => {
+    const currency = await CurrencyModel.findByPk(req.body.currencyID);
+    if (!currency) {
+        return res.status(404).json({
+            success: false,
+            error: 'Currency not found.',
+        });
+    }
+
+    if (!req.body.currencyName && !req.body.currencySymbol) {
+        return res.status(400).json({
+            success: false,
+            error: 'Please provide a valid currencyName or currencySymbol.',
+        });
+    }
+
+    try {
+        const updatedCurrency = await currency.update({
+            currencyName: req.body.currencyName,
+            currencySymbol: req.body.currencySymbol,
+        });
+        res.json({
+            success: true,
+            data: updatedCurrency,
+        });
+    } catch (error) {
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json({
+                success: false,
+                error: `Currency ${error.errors[0].value} already exists`,
+            });
+        }
+
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
 module.exports = router;
