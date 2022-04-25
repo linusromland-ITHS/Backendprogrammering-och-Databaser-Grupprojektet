54cd021a60499197254555c76da41d1b6793181e
Linus Romland <romlandlinus@gmail.com>
2022-04-25 09:10:31 +0200

    Created edit city route

--- backend/routes/city.routes.js
+++ backend/routes/city.routes.js
@@ -40,4 +40,44 @@ router.post('/', async (req, res) => {
     }
 });
 
+/**
+ * @api {put} /api/city/ Change a city based on id
+ */
+router.put('/', async (req, res) => {
+    const { cityID, cityName, cityPopulation } = req.body;
+    if (!cityName && !cityName.length < 1 && !cityPopulation) {
+        return res.status(400).json({
+            success: false,
+            error: 'Please provide a name and/or population.',
+        });
+    }
+
+    try {
+        const foundCity = await CityModel.findOne({ where: { cityID } });
+
+        if (!foundCity) {
+            res.status(404).json({
+                success: false,
+                error: `City with id ${cityID} could not be found`,
+            });
+        }
+
+        const updatedCity = await foundCity.update({ cityName: cityName, cityPopulation: cityPopulation });
+
+        res.json({ success: true, error: '', data: updatedCity });
+    } catch (error) {
+        if (error.name === 'SequelizeUniqueConstraintError') {
+            return res.status(400).json({
+                success: false,
+                error: `The city ${cityName} already exists.`,
+            });
+        }
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            error: error.message,
+        });
+    }
+});
+
 module.exports = router;
