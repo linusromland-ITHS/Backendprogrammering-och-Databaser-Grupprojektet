1e2fbe344673d90033ef13243580ff0df6b8784b
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2022-04-25 14:13:57 +0200

    Create delete route for currency

--- backend/routes/currency.routes.js
+++ backend/routes/currency.routes.js
@@ -82,4 +82,42 @@ router.put('/', async (req, res) => {
     }
 });
 
+/**
+ * @api {delete} /api/city/ Delete a city
+ */
+router.delete('/', async (req, res) => {
+    const { currencyID } = req.body;
+
+    if (!currencyID) {
+        return res.status(400).json({
+            success: false,
+            error: 'Please provide a currency id.',
+        });
+    }
+
+    try {
+        const deletedCurrency = await CurrencyModel.destroy({ where: { currencyID } });
+
+        if (!deletedCurrency) {
+            return res.status(404).json({
+                success: false,
+                error: `Currency with id ${currencyID} could not be found`,
+            });
+        }
+
+        res.json({ success: true, error: '', data: deletedCurrency });
+    } catch (error) {
+        if (error.name === 'SequelizeForeignKeyConstraintError') {
+            return res.status(400).json({
+                success: false,
+                error: `Currency with id ${currencyID} could not be deleted because it is being used as a currency in a country.`,
+            });
+        }
+        res.status(500).json({
+            success: false,
+            error: error.message || `Could not delete currency with id ${currencyID}`,
+        });
+    }
+});
+
 module.exports = router;
