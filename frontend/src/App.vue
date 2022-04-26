22b15261c3d8104846246a3be1dcf5136900a091
Linus Romland <romlandlinus@gmail.com>
2022-04-26 10:50:03 +0200

    Created navbar with fixed change (from data)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,14 @@
-<template><RouterView /></template>
+<template>
+	<Navbar />
+	<RouterView />
+</template>
+<script>
+	import Navbar from './components/Navbar.vue';
+
+	export default {
+		name: 'App',
+		components: {
+			Navbar,
+		},
+	};
+</script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+	<!-- Navbar Component -->
+	<nav :class="`pt-8 flex justify-center bg-${activeTheme}`">
+		<a href="result.html" :class="`link link-${activeTheme}  ${activeTheme == 'land' ? 'active-link' : ''}`"
+			><span class="material-icons mr-1">flag</span>Land</a
+		>
+		<a href="index.html" :class="`link link-${activeTheme} ${activeTheme == 'sea' ? 'active-link' : ''}`"
+			><span class="material-icons mr-1">sailing</span>Sea</a
+		>
+		<a href="details.html" :class="`link link-${activeTheme}  ${activeTheme == 'space' ? 'active-link' : ''}`"
+			><span class="material-icons mr-1">public</span>Space</a
+		>
+	</nav>
+</template>
+
+<script>
+	export default {
+		name: 'Navbar',
+		data() {
+			return {
+				activeTheme: 'sea',
+			};
+		},
+	};
+</script>
+<style>
+	.link {
+		@apply py-5 font-semibold text-white flex flex-row justify-center rounded-t-md transition ease-out duration-150 w-1/3 sm:w-32 hover:ease-in;
+	}
+
+	.link-sea {
+		@apply bg-sky-500 hover:bg-sky-400;
+	}
+
+	.link-land {
+		@apply bg-emerald-500 hover:bg-emerald-400;
+	}
+
+	.link-space {
+		@apply bg-purple-500 hover:bg-purple-400;
+	}
+
+	.active-link {
+		@apply bg-white text-black hover:bg-white hover:text-black;
+	}
+</style>
