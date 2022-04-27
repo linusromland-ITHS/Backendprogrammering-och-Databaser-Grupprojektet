0c527930de9d7705a2b8dddbb152525628f6f32c
vikiorf <viktor.johansson96@gmail.com>
2022-04-27 09:00:13 +0200

    added Result.vue

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -15,6 +15,12 @@ const router = createRouter({
 			component: () => import('./views/Search.vue'),
 			path: '/:type',
 		},
+		{
+			// Search Route
+			name: 'Result',
+			component: () => import('./views/Result.vue'),
+			path: '/result/:type',
+		},
 	],
 });
 
---
+++ frontend/src/views/Result.vue
@@ -0,0 +1,189 @@
+<template>
+	<Navbar />
+	<div class="w-2/3 site-height mx-auto flex justify-between">
+		<!-- Remove when SearchComponent is ready -->
+		<form class="w-1/6">
+			<formset class="block my-5">
+				<div class="relative text-gray-600 focus-within:text-gray-400 w-full">
+					<span class="absolute inset-y-0 left-0 flex items-center pl-2">
+						<button type="submit" class="p-1 grid place-content-center">
+							<span class="material-icons text-black">search</span>
+						</button>
+					</span>
+					<input
+						type="search"
+						class="py-2 text-sm text-black bg-gray-200 rounded-md pl-10 w-full transition focus:outline-none hover:bg-gray-100 focus:bg-gray-300 ease-in duration-150 border-2 border-gray-300"
+						placeholder="Search..."
+						autocomplete="off"
+					/>
+				</div>
+			</formset>
+			<div class="my-5">
+				<formset class="w-full inline-block mb-4">
+					<label class="font-bold flex flex-col">
+						Amount of moons:
+						<label class="font-semibold flex justify-between my-1"
+							><p>Min:</p>
+							<input type="number" min="0" max="100" value="0" class="w-3/4"
+						/></label>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Max:</p>
+							<input type="number" min="0" max="100" value="100" class="w-3/4"
+						/></label>
+					</label>
+				</formset>
+				<formset class="w-full inline-block mb-4">
+					<label class="font-bold flex flex-col">
+						<p>Radius <span class="help-text">(km)</span></p>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Min:</p>
+							<input type="number" min="0" max="100" value="0" class="w-3/4"
+						/></label>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Max:</p>
+							<input type="number" min="0" max="100" value="100" class="w-3/4"
+						/></label>
+					</label>
+				</formset>
+				<formset class="w-full inline-block mb-4">
+					<label class="font-bold flex flex-col">
+						<p>Orbital period <span class="help-text">(days)</span></p>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Min:</p>
+							<input type="number" min="0" max="100" value="0" class="w-3/4"
+						/></label>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Max:</p>
+							<input type="number" min="0" max="100" value="100" class="w-3/4"
+						/></label>
+					</label>
+				</formset>
+				<formset class="w-full inline-block">
+					<label class="font-bold flex flex-col">
+						<p>Distance from Sun <span class="help-text">(km)</span></p>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Min:</p>
+							<input type="number" min="0" max="100" value="0" class="w-3/4"
+						/></label>
+						<label class="font-semibold flex justify-between my-1"
+							><p>Max:</p>
+							<input type="number" min="0" max="100" value="100" class="w-3/4"
+						/></label>
+					</label>
+				</formset>
+			</div>
+		</form>
+		<div class="w-9/12 h-full p-3 flex flex-col">
+			<h2 class="text-2xl font-semibold">Results:</h2>
+
+			<ul class="grow overflow-y-scroll my-3">
+				<!-- Add when ResultComponent is ready -->
+				<!-- <ResultComponent v-for="result in results" :key="result.id ? result.id : result._id" :result="result" /> -->
+
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Sweden</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Norway</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Finland</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Iceland</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Spain</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Germany</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">United Kingdom</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">United States</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Canada</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Australia</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Japan</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Ukraine</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">Poland</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">China</h3>
+				</li>
+				<li
+					class="w-3/5 bg-gray-200 rounded-md p-2 hover:bg-gray-300 transition ease-in duration-150 cursor-pointer mb-4 hover:drop-shadow-md"
+				>
+					<h3 class="text-lg font-bold">India</h3>
+				</li>
+			</ul>
+			<p class="">Showing 15 out of 196 results</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Navbar from '../components/Navbar.vue';
+	export default {
+		components: { Navbar },
+		data() {
+			return {
+				type: '',
+			};
+		},
+		computed: {
+			// Return results from store based on type
+			results() {
+				return [];
+			},
+		},
+		created() {
+			this.type = this.$route.params.type;
+		},
+	};
+</script>
+
+<style scoped>
+	.site-height {
+		height: calc(100vh - 96px);
+	}
+</style>
