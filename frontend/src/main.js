c5e03880f0e2c1ed22614d89a4781bd7f9547c61
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-04-30 14:15:44 +0200

    imported `vue-axios`
    Co-Authored-By: Linus Romland <hello@linusromland.com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,7 @@
 //External Dependencies import:
 import { createApp } from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 //Internal Dependencies import:
 import App from './App.vue';
@@ -11,6 +13,9 @@ const app = createApp(App);
 
 //Registers vue-router
 app.use(router);
+
+//Registers vue-axios
+app.use(VueAxios, axios);
 
 //Mounts app to div with id app
 app.mount('#app');
