e429831e22841519bf216b0f80238d6a29f25457
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-04-30 12:26:27 +0200

    created `Form.vue`

---
+++ frontend/src/components/Form.vue
@@ -0,0 +1,16 @@
+<template>
+	<form @submit.prevent="" />
+</template>
+
+<script>
+	export default {
+		name: 'Form',
+		props: {
+			method: {
+				type: String,
+				required: true,
+				validator: (value) => ['post', 'put', 'delete'].includes(value),
+			},
+		},
+	};
+</script>
