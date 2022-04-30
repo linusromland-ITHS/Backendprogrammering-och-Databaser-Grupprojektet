10c70e632c39e526a08be3379b97cc58c2daa6c8
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-04-30 15:46:27 +0200

    created navigation tabs in `Admin.vue`
    Co-Authored-By: Linus Romland <hello@linusromland.com>

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -10,10 +10,15 @@ const router = createRouter({
 			path: '/',
 		},
 		{
+			// Admin Redirect Route
+			path: '/admin',
+			redirect: '/admin/city',
+		},
+		{
 			// Admin Route
 			name: 'Admin',
 			component: () => import('./views/Admin.vue'),
-			path: '/admin',
+			path: '/admin/:tab',
 		},
 		{
 			// Search Route
--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -1,9 +1,45 @@
 <template>
 	<h1 class="text-3xl text-red-400">Admin</h1>
+	<nav>
+		<button
+			v-for="(tab, index) in tabs"
+			:key="index"
+			:class="{ 'button-active': tab.toLowerCase() == activeTab.toLowerCase() }"
+			@click="setActiveTab(tab)"
+		>
+			{{ tab }}
+		</button>
+	</nav>
 </template>
 <script>
 	export default {
 		name: 'Admin',
+		data() {
+			return {
+				tabs: ['City', 'Continent', 'Country', 'Currency', 'Language', 'Planet', 'Religion', 'Sea'],
+			};
+		},
+		computed: {
+			activeTab() {
+				return this.$route.params.tab;
+			},
+		},
+		methods: {
+			setActiveTab(tab) {
+				this.$router.push(`/admin/${tab.toLowerCase()}`);
+			},
+		},
 	};
 </script>
-<style></style>
+<style scoped>
+	nav {
+		@apply bg-gray-200;
+	}
+
+	button {
+		@apply px-5 py-2 bg-gray-200 rounded-t-md hover:bg-gray-300 transition ease-out duration-150;
+	}
+	.button-active {
+		@apply bg-blue-500 text-white hover:bg-blue-600;
+	}
+</style>
