5fab860039c9e3445433a5bd299764a50d9d097f
svante-jonsson <77492340+svante-jonsson@users.noreply.github.com>
2022-04-30 16:58:37 +0200

    created function to delete an item from the database
    Co-Authored-By: Linus Romland <hello@linusromland.com>

--- frontend/src/views/Admin.vue
+++ frontend/src/views/Admin.vue
@@ -4,25 +4,42 @@
 		<button
 			v-for="(tab, index) in tabs"
 			:key="index"
-			:class="{ 'button-active': tab.toLowerCase() == activeTab.toLowerCase() }"
-			@click="setActiveTab(tab)"
+			:class="{ 'button-active': tab == activeTab.label }"
+			@click="updateActiveTab(tab)"
 		>
 			{{ tab }}
 		</button>
 	</nav>
+	<div>
+		<button>Create {{ activeTab.label }}</button>
+		<ul>
+			<li v-for="(item, index) in data" :key="index">
+				<p>{{ item[`${activeTab.value}Name`] }}</p>
+				<button :title="`Edit ${activeTab.label}`"><span class="material-icons"> edit </span></button>
+				<button @click="deleteItem(item)" :title="`Delete ${activeTab.label}`">
+					<span class="material-icons"> delete </span>
+				</button>
+			</li>
+		</ul>
+	</div>
 </template>
 <script>
+	import { useToast } from 'vue-toastification';
+
 	export default {
 		name: 'Admin',
 		data() {
 			return {
 				tabs: ['City', 'Continent', 'Country', 'Currency', 'Language', 'Planet', 'Religion', 'Sea'],
+				activeTab: {
+					label: '',
+					value: '',
+				},
+				data: [],
+				confirmDelete: '',
+				confirmDeleteTimeout: null,
+				confirmDeleteToast: null,
 			};
-		},
-		computed: {
-			activeTab() {
-				return this.$route.params.tab;
-			},
 		},
 		watch: {
 			$route() {
@@ -33,14 +50,63 @@
 			this.getData();
 		},
 		methods: {
-			setActiveTab(tab) {
+			updateActiveTab(tab) {
 				this.$router.push(`/admin/${tab.toLowerCase()}`);
 			},
 			async getData() {
-				await this.axios({
+				const tab = this.$route.params.tab.toLowerCase();
+				this.activeTab.label = tab.charAt(0).toUpperCase() + tab.slice(1);
+				this.activeTab.value = tab;
+
+				const request = await this.axios({
 					method: 'get',
-					url: this.activeTab.toLowerCase(),
+					url: this.activeTab.value,
 				});
+
+				this.data = (await request.data).data;
+			},
+			async deleteItem(item) {
+				const toast = useToast();
+
+				const id = item[item._id ? '_id' : `${this.activeTab.value}ID`];
+
+				if (this.confirmDelete != id) {
+					this.confirmDelete = id;
+					toast.dismiss(this.confirmDeleteToast);
+
+					this.confirmDeleteToast = toast.info(
+						`To confirm, click the delete button again within 10 seconds.`,
+						{
+							timeout: 10000,
+							hideProgressBar: false,
+						},
+					);
+
+					clearTimeout(this.confirmDeleteTimeout);
+					this.confirmDeleteTimeout = setTimeout(() => {
+						this.confirmDelete = '';
+					}, 10000);
+				} else {
+					toast.dismiss(this.confirmDeleteToast);
+					clearTimeout(this.confirmDeleteTimeout);
+
+					const request = await this.axios({
+						method: 'delete',
+						url: this.activeTab.value,
+						data: {
+							id: id,
+						},
+					});
+
+					const response = await request.data;
+
+					if (response.success) {
+						this.getData();
+						toast.success(`${this.activeTab.label} deleted.`);
+					} else {
+						toast.error(`Error: ${response.error}.`);
+					}
+				}
 			},
 		},
 	};
@@ -50,7 +116,7 @@
 		@apply bg-gray-200;
 	}
 
-	button {
+	nav > button {
 		@apply px-5 py-2 bg-gray-200 rounded-t-md hover:bg-gray-300 transition ease-out duration-150;
 	}
 	.button-active {
